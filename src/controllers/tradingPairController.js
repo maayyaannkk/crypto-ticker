c63660d7512822b8b827b7ddf8f71f03dd2f43fd
Mayank <maayyaannkk@gmail.com>
2020-10-01 03:27:30 +0530

    fix: validation cleanup

--- src/controllers/tradingPairController.js
+++ src/controllers/tradingPairController.js
@@ -10,37 +10,17 @@ redisClient.on('error', (err) => {
 });
 
 exports.getTradingPairValues = (req, response) => {
-    checkExchangeInJson(req.body.tradingPairs).then(
-        _ => {
-            getValues(JSON.parse(req.body.tradingPairs)).then(
-                result => {
-                    response.status(200).json({ "success": true, "pairs": result });
-                }
-            );
+    getValues(req.body.tradingPairs).then(
+        result => {
+            response.status(200).json({ "success": true, "pairs": result });
         },
         _ => { response.status(200).json({ "error": "invalid json" }); }
     );
 };
 
-function checkExchangeInJson(str) {
-    return new Promise(function (resolve, reject) {
-        try {
-            var arr = JSON.parse(str);
-            redisClient.smembers('exchangeList', function (err, reply) {
-                Object.keys(arr).forEach(function (key) {
-                    if (!reply.includes(key)) {
-                        reject();
-                    }
-                });
-                resolve();
-            });
-        } catch (e) {
-            reject();
-        }
-    });
-}
-
 async function getValues(requestedPairs) {
+    requestedPairs = await checkExchangeInJson(requestedPairs).catch(err => { });
+
     var exchangePromises = [];
     var resultToReturn = {};
     Object.keys(requestedPairs).forEach(function (key) {
@@ -61,6 +41,24 @@ async function getValues(requestedPairs) {
         });
         resultToReturn[exchangeName] = exchangePairValues;
     });
-    
+
     return JSON.parse(JSON.stringify(resultToReturn));
+}
+
+function checkExchangeInJson(str) {
+    return new Promise(function (resolve, reject) {
+        try {
+            var arr = JSON.parse(str);
+            redisClient.smembers('exchangeList', function (err, reply) {
+                Object.keys(arr).forEach(function (key) {
+                    if (!reply.includes(key)) {
+                        reject();
+                    }
+                });
+                resolve(JSON.parse(str));
+            });
+        } catch (e) {
+            reject();
+        }
+    });
 }
